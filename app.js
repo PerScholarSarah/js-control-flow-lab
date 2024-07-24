dc376ffa1983ed568185d27f4e08589591b3ce8f
PerScholarSarah <rmandolin22@gmail.com>
2024-07-24 11:47:12 -0400

    done

--- app.js
+++ app.js
@@ -3,33 +3,57 @@ const radius = 5;
 const minSpacePerPlant = 0.8;
 const growthRate = 2;
 
-const area = pi * radius * radius;
-const maxPlants = area / minSpacePerPlant;
+const plantingArea = pi * radius * radius;
 
 const predictGrowth = (initialPlantCount, weeks) =>{
     let plants = initialPlantCount;
     for (let t = 0; t < weeks; t++) {
         plants *= growthRate;
     }
-    console.log(`There will be ${plants} after ${weeks}. Plants should be:`);
-    if(plants > 0.8 * maxPlants){
-        console.log("Pruned");
-    }
-    else if(plants >= 0.5 * maxPlants){
-        console.log("Monitored");
-    }
-    else{
-       console.log("Planted");
-    }
     return plants;
 }
 
-predictGrowth(20, 1);
-predictGrowth(20, 2);
-predictGrowth(20, 3);
+const giveAdvice = (plants) =>
+{
+    try {
+        let minArea = plants * minSpacePerPlant;
+        if(minArea > 0.8 * plantingArea){
+            console.log("Pruned");
+        }
+        else if(minArea >= 0.5 * plantingArea){
+            console.log("Monitored");
+        }
+        else{
+        console.log("Planted");
+        }
+        if(minArea > plantingArea){
+            throw(`SoilOverflowError: ${plants} plants need ${minArea}m2! We only have ${plantingArea}m2...`);
+        }
+    } catch (msg) {
+        console.error(msg);
+    }
+    
+}
 
-// -------------------Part 2-------------------
+const neededRadius = (numPlants) => {
+    let newArea = numPlants * minSpacePerPlant;
+    let newRadius = Math.sqrt(newArea / pi);
+    return newRadius;
+}
 
-let neededSpace = predictGrowth(100, 10);
-let newRadius = Math.sqrt(neededSpace/pi);
-console.log(object);
+let oneweek = predictGrowth(20, 1);
+giveAdvice(oneweek);
+
+let twoweeks = predictGrowth(20, 2);
+giveAdvice(twoweeks);
+
+let threeweeks = predictGrowth(20, 3);
+giveAdvice(threeweeks);
+
+
+let newProposal = predictGrowth(100, 10);
+let expandedRadius = neededRadius(newProposal);
+
+console.log(`If you start with 100 plants, after 10 weeks you'll need a circle with radius ${expandedRadius}m`);
+
+giveAdvice(newProposal);
